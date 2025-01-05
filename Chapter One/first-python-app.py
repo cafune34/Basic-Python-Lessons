f7c42595981a5344ebfa1ffb1f966a4661c1432f
Hakan ツ <41235163+cafune34@users.noreply.github.com>
2025-01-05 22:00:27 +0300

    Add files via upload

---
+++ Chapter One/first-python-app.py
@@ -0,0 +1,15 @@
+# Konsola Bir Yazi Gonderdik
+
+print("Merhaba -cafune344- Takipcileri")
+
+# Konsoldan Kullanici Tarafindan Gonderilecek Olan Degeri Ekrana Yazdiracagiz
+
+print("Lutfen Adinizi Giriniz: ")
+
+isim = input()
+
+# Ekrana Gosterilecek Degerin Iki Farkli Yazim Turunu Gosterecegiz
+
+print("Hosgeldin: " + isim)
+
+print(f"Hosgeldin: {isim}")
